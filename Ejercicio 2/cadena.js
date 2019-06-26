8bc23a4d73d1edb24ad61c3ba3dc1775b1c1bcb5
ericromang <44595368+ericromang@users.noreply.github.com>
2019-06-26 09:23:47 -0600

    cambios
    asd

--- Ejercicio 2/cadena.js
+++ Ejercicio 2/cadena.js
@@ -8,23 +8,10 @@ function letras(str){
     return vocales(str)+consonantes(str);
 }
 function vocales(str){
-    var result=0;
-        for(var letterIndex in str){
-            if(str[letterIndex].toLowerCase()== 'a' || str[letterIndex].toLowerCase()== 'e' || str[letterIndex].toLowerCase()== 'i'
-            ||  str[letterIndex].toLowerCase()== 'o' || str[letterIndex].toLowerCase()== 'u')
-                result++;
-        }
-        return result;
+        return str.match(/[aeiou]/gi).length;
 }
 function consonantes(str){
-    var result=0;
-    str = str.replace(/\s+/g,'');
-        for(var letterIndex in str){
-            if(str[letterIndex].toLowerCase()!= 'a' && str[letterIndex].toLowerCase()!= 'e' && str[letterIndex].toLowerCase()!= 'i'
-            && str[letterIndex].toLowerCase()!= 'o' && str[letterIndex].toLowerCase()!= 'u')
-                result++;
-        }
-        return result;
+    return str.match(/[bdfghjklmnñpqrstvxyz]/gi).length;
 }
 
 module.exports.separar = separar;
